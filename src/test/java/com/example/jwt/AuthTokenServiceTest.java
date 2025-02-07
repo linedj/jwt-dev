9ca7e61366836584161ea261f4240d2f4536a450
linedj <eunjun998@gmail.com>
2025-02-07 14:10:07 +0900

    jwt util

--- src/test/java/com/example/jwt/AuthTokenServiceTest.java
+++ src/test/java/com/example/jwt/AuthTokenServiceTest.java
@@ -1,12 +1,17 @@
 package com.example.jwt;
 
 import com.example.jwt.domain.member.member.service.AuthTokenService;
+import com.example.jwt.standard.util.Ut;
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.security.Key;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,8 +25,20 @@ public class AuthTokenServiceTest {
 
     @Test
     @DisplayName("AuthTokenService 생성")
-    void init(){
+    void init() {
         assertThat(authTokenService).isNotNull();
-
     }
+
+    @Test
+    @DisplayName("jwt 생성")
+    void createToken() {
+        int expireSeconds = 60 * 60 * 24 * 365;
+        Key secretKey = Keys.hmacShaKeyFor("abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890".getBytes());
+
+        String jwt = Ut.Jwt.createToken(secretKey, expireSeconds, Map.of("name", "john", "age", 23));
+        assertThat(jwt).isNotBlank();
+
+        System.out.println("jwt = " + jwt);
+    }
+
 }
