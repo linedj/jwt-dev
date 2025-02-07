2943a2a901fce288190f0864d6cc837d93be8673
linedj <eunjun998@gmail.com>
2025-02-07 12:29:28 +0900

    authTokenService & jwt

---
+++ src/main/java/com/example/jwt/domain/member/member/service/AuthTokenService.java
@@ -0,0 +1,10 @@
+package com.example.jwt.domain.member.member.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthTokenService {
+
+}
