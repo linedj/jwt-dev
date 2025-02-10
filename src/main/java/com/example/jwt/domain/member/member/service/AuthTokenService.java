f3fff5df799adfbc6f66922897def5a744987d64
linedj <eunjun998@gmail.com>
2025-02-10 10:45:01 +0900

    custom.jwt.value

--- src/main/java/com/example/jwt/domain/member/member/service/AuthTokenService.java
+++ src/main/java/com/example/jwt/domain/member/member/service/AuthTokenService.java
@@ -2,8 +2,8 @@ package com.example.jwt.domain.member.member.service;
 
 import com.example.jwt.domain.member.member.entity.Member;
 import com.example.jwt.standard.util.Ut;
-import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -12,14 +12,31 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class AuthTokenService {
 
+    @Value("${custom.jwt.secret-key}")
+    private String keyString;
+
+    @Value("${custom.jwt.expire-seconds}")
+    private int expireSeconds;
+
     public String genAccessToken(Member member) {
 
-        int expireSeconds = 60 * 60 * 24 * 365;
-
         return Ut.Jwt.createToken(
-                Keys.hmacShaKeyFor("abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890".getBytes()),
+                keyString,
                 expireSeconds,
                 Map.of("id", member.getId(), "username", member.getUsername())
         );
+    }
+
+    public Map<String, Object> getPayload(String token) {
+        Map<String, Object> payload = Ut.Jwt.getPayload(keyString, token);
+
+        if(payload == null) return null;
+
+        Number idNo = (Number)payload.get("id");
+        long id = idNo.longValue();
+
+        String username = (String)payload.get("username");
+
+        return Map.of("id", id, "username", username);
     }
 }
