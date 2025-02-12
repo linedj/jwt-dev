b9a214d92818db0c367e469dcfbede82039f175f
linedj <eunjun998@gmail.com>
2025-02-12 09:16:14 +0900

    exclude url in filter

--- src/main/java/com/example/jwt/global/security/CustomAuthenticationFilter.java
+++ src/main/java/com/example/jwt/global/security/CustomAuthenticationFilter.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -84,6 +85,13 @@ public class CustomAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String url = request.getRequestURI();
+
+        if(List.of("/api/v1/members/login", "/api/v1/members/join").contains(url)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
         AuthToken tokens = getAuthTokenFromRequest();
 
